624214770026dfb3ad11b3e53adc1ba99332e8ad
HussienElbatran <143851361+HussienElbatran@users.noreply.github.com>
2026-05-11 20:41:53 +0300

    Update Query.sql

--- -Practical exam using SQL Server/Query.sql
+++ -Practical exam using SQL Server/Query.sql
@@ -4,14 +4,6 @@ Marks Distribution:
 Query 1: Display student names and emails → 2 Marks
 Query 2: Display courses with hours greater than 60 → 2 Marks
 Query 3: Display student names with course codes and grades using JOIN → 2 Marks
-Question:
-
-Write SQL queries to perform the following:
-
-Display all student names and emails.
-Display all courses where hours are greater than 60.
-Display student names, course codes, and grades using JOIN.
-
 
 */
 
