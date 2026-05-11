e469767133e0931dde75059a1fe4714ae4b5dde4
HussienElbatran <143851361+HussienElbatran@users.noreply.github.com>
2026-05-11 20:39:59 +0300

    Insert sample data into students, courses, and enrollments
    Added DML statements to insert sample data for students, courses, and enrollments.

--- -Practical exam using Mysql/dml.sql
+++ -Practical exam using Mysql/dml.sql
@@ -1,3 +1,25 @@
+
+/*
+Question 2: (DML – INSERT) – 6 Marks
+
+Insert the following data into the tables:
+
+Students:
+(1, "Ahmed Ali", 20, "ahmed@mail.com")
+(2, "Sara Mohamed", 22, "sara@mail.com")
+
+Courses:
+(101, "Database", 3)
+(102, "Programming", 4)
+
+
+Enrollments:
+(1, 101, 85)
+(2, 102, 90)
+
+*/
+
+
 USE student_sys;
 
 -- Insert Students
